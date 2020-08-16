74f7772967c7fe6efc165ec206da21213765c84c
MarshesDuck <mail@megzhang.com>
2020-08-16 19:47:17 -0400

    Update Course Monitor.js
    Formatting

--- Course Monitor.js
+++ Course Monitor.js
@@ -20,8 +20,8 @@ courseNumber = "1201E"
 // ----------------------------///------------------------------------
 
 // Asks for permission to play a notification
-function grantNotifPerm(){
-  if (Notification.permission !== "granted"){
+function grantNotifPerm() {
+  if (Notification.permission !== "granted") {
     Notification.requestPermission();
   }
 }
@@ -39,7 +39,7 @@ if (window.history.replaceState) {
 }
 
 // automatically clicks submit button to refresh the page
-setTimeout(function(){
+setTimeout(function() {
   $(".btn.btn-info.span2").trigger("click");
 }, 10000);
 
@@ -49,58 +49,51 @@ var COLOR = "red"
 var courseAlert = false;
 
 // highlights and parses text
-var allText = document.evaluate
-( "//text()[contains(., '" + TEXT + "' )]", document,
-null, XPathResult. ORDERED_NODE_SNAPSHOT_TYPE , null);
+var allText = document.evaluate("//text()[contains(., '" + TEXT + "' )]", document,
+  null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-for(var i = 0; i < allText.snapshotLength; i++)
-{
-	var cur = allText.snapshotItem(i);
-	var par = cur.parentNode;
-	var textInd;
-	var curName = cur.nodeName;
+for (var i = 0; i < allText.snapshotLength; i++) {
+  var cur = allText.snapshotItem(i);
+  var par = cur.parentNode;
+  var textInd;
+  var curName = cur.nodeName;
 
-  do
-	{
-		var curText = cur.nodeValue;
-		textInd = curText.indexOf(TEXT);
+  do {
+    var curText = cur.nodeValue;
+    textInd = curText.indexOf(TEXT);
 
-    if(textInd != -1)
-		{
-			var before = document.createTextNode( curText.substring(0, textInd ) );
-			var highlight = document. createElement("span");
-			highlight.class = "highlight";
-			highlight.textContent = TEXT;
-			highlight.style.color = COLOR;
-			var after = document.createTextNode
-      ( curText.substring(textInd + TEXT.length) );
+    if (textInd != -1) {
+      var before = document.createTextNode(curText.substring(0, textInd));
+      var highlight = document.createElement("span");
+      highlight.class = "highlight";
+      highlight.textContent = TEXT;
+      highlight.style.color = COLOR;
+      var after = document.createTextNode(curText.substring(textInd + TEXT.length));
 
-			par.insertBefore(before, cur);
-			par.insertBefore(highlight, cur);
-			par.insertBefore(after, cur);
-			par.removeChild(cur);
-			cur = after;
-      // sets courseAlert to trigger a notification
+      par.insertBefore(before, cur);
+      par.insertBefore(highlight, cur);
+      par.insertBefore(after, cur);
+      par.removeChild(cur);
+      cur = after;
+      // sets courseAlert to true to trigger a notification
       courseAlert = true;
-		}
-	} while(textInd != -1)
+    }
+  } while (textInd != -1)
 }
 
 // Creates the notification to be shown when a course is not full.
-function notifyMe(){
-  var notification = new Notification
-  (courseSubject + " " + courseNumber + " is now avaliable!");
+function notifyMe() {
+  var notification = new Notification(courseSubject + " " + courseNumber + " is now avaliable!");
 }
 
 // Notification sound audio
-function play(){
-  var audio = new Audio
-  ("https://raw.githubusercontent.com/MarshesDuck/Course-Monitor/master/insight.mp3");
+function play() {
+  var audio = new Audio("https://raw.githubusercontent.com/MarshesDuck/Course-Monitor/master/insight.mp3");
   audio.play();
 }
 
 // Sends out the notification when a course is avaliable
-if (courseAlert == true){
+if (courseAlert == true) {
   notifyMe();
   play();
 }
